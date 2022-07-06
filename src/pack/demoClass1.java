0fc560bdb0ece9efc6b9517a893c520613f36351
HP <HP@LAPTOP-06RA4R2G>
2022-07-06 20:05:18 +0530

    Demo class1 branchB commit

---
+++ src/pack/demoClass1.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class demoClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hellow There Again");	
+	}
+
+}
